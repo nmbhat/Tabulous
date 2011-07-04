b2a80034099e7df165103a2ed68a463779e845f0
Nish <nishant.bhat@gmail.com>
2011-07-03 21:46:51 -0700

    some syntax fixes

--- authScript.js
+++ authScript.js
@@ -2,5 +2,5 @@ var auth = document.getElementById('authID').innerHTML;
 if (auth) {
 	chrome.extension.sendRequest({target: 'bg', authID: auth});
 }
-document.getElementById('status').innerHTML = "You're logged in and ready to start syncing!"
-document.getElementById('authID').innerHTML = "You may close this page now."
+document.getElementById('status').innerHTML = "You're logged in and ready to start syncing!";
+document.getElementById('authID').innerHTML = "You may close this page now.";
